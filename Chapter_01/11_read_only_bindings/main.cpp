9e7691324f89f59c5ff6df4d1e56af810a744ba4
Imil Ametov <charlz_klug@mail.ru>
2023-12-04 12:54:52 +0300

    Finished Read-Only Bindings

--- Chapter_01/11_read_only_bindings/main.cpp
+++ Chapter_01/11_read_only_bindings/main.cpp
@@ -1,11 +1,24 @@
-#include "customer1.hpp"
+// #include "customer1.hpp"
+#include "structbind1.hpp"
 #include <iostream> // For std::cout
 
 int main() {
-    Customer myCustomer {"Vasya", "Pupkin", 49};
-    std::cout << myCustomer.getFirst() << std::endl;
-    std::cout << myCustomer.getLast() << std::endl;
-    std::cout << myCustomer.getValue() << std::endl;
+    Customer c {"Tim", "Starr", 42};
+    auto [f, l, v] = c;
+    std::cout << "f/l/v: " << f << ' ' << l << ' ' << v << std::endl;
     
+    std::string s= std::move(f);
+    l= "Waters";
+    v += 10;
+    std::cout << "f/l/v: " << f << ' ' << l << ' ' << v << std::endl;
+    std::cout << "c: " << c.getFirst() << ' '
+	      << c.getLast() << ' ' << c.getValue() << std::endl;
+    std::cout << "s: " << s << std::endl;
+
+    int k= 79;
+    std::cout << "k: " << k << std::endl;
+    int t= std::move(k);
+    std::cout << "t: " << t << std::endl;
+    std::cout << "k: " << k << std::endl;
     return 0;
 }
--- Chapter_01/11_read_only_bindings/structbind1.hpp
+++ Chapter_01/11_read_only_bindings/structbind1.hpp
@@ -18,8 +18,18 @@ struct std::tuple_element<Idx, Customer> {
     using type = std::string;
 };
 
-template<std::size_t> auto get(const Customer& c);
-template<> auto get<0>(const Customer& c) {return c.getFirst();}
-template<> auto get<1>(const Customer& c) {return c.getLast();}
-template<> auto get<2>(const Customer& c) {return c.getValue();}
+// template<std::size_t> auto get(const Customer& c);
+// template<> auto get<0>(const Customer& c) {return c.getFirst();}
+// template<> auto get<1>(const Customer& c) {return c.getLast();}
+// template<> auto get<2>(const Customer& c) {return c.getValue();}
+template<std::size_t I> auto get(const Customer& c) {
+    static_assert(I < 3);
+    if constexpr (I == 0) {
+	return c.getFirst();
+    } else if constexpr (I == 1) {
+	return c.getLast();
+    } else {
+	return c.getValue();
+    }
+}
 #endif // STRUCTBIND1_HPP
